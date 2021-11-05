d1f5b213e0d5adbd2731bee318848bc1ab80bc20
viknedus <eduvanvictor@yahoo.com>
2021-11-05 14:12:58 +0100

    update

--- keys.js
+++
@@ -1,5 +0,0 @@
-// API key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyDveSphamSm1GUnVB8Q3YX7U16XzE92kXo";
-
-// Context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = "8ada322e061751f41";
