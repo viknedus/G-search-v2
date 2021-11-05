8999f879e85520995f193a916ca4a2bf6a7c849e
viknedus <eduvanvictor@yahoo.com>
2021-11-05 17:06:43 +0100

    update

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,4 +1,7 @@
+import { useRouter } from "next/router";
+
 function Avatar({ className }) {
+  const router = useRouter();
   return (
     <img
       className={`h-8 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
